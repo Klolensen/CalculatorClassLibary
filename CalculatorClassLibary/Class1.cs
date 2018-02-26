09aea9d4218b145e075e1b83a64f46192f3d40fa
Klolensen <31515047+Klolensen@users.noreply.github.com>
2018-02-26 09:43:42 +0100

    Add project files.

---
+++ CalculatorClassLibary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClassLibary
+{
+    public class Class1
+    {
+    }
+}
